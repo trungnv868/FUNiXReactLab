7c0046abd1534e0ae370d448145dc70dbe0ba1e8
Trung Nguyen <trungnv.ht06@gmail.com>
2021-12-31 21:56:32 +0700

    Commit 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 import './App.css';
 
 
@@ -16,15 +17,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        <Navbar dark color='primary'>
-          <div className='container'>
-            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+      <div className="App">
+        <Main />
       </div>
-    )
+    );
   }
 }
 
