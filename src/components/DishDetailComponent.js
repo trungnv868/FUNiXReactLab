93ec501ea553c7f3c55a4cb0cd98b51fee0ffe7f
Trung Nguyen <trungnv.ht06@gmail.com>
2022-02-25 20:02:57 +0700

    Commit lab08_1

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -34,7 +34,9 @@ function RenderDish({dish}) {
         </div> 
     );
 }
-function RenderComments({comments}) {
+
+
+  function RenderComments({comments, addComment, dishId}) {
     if (comments != null)
         return (
             <div className ="col-12 col-md-5 m-1">
@@ -49,7 +51,7 @@ function RenderComments({comments}) {
                         )
                     })}
                 </ul>
-                <CommentForm />            
+                <CommentForm dishId={dishId} addComment={addComment} />        
             </div>
         );
     else
@@ -77,8 +79,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    
-    alert('Current State is: ' + JSON.stringify(values));
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
   }
   
   render() {
@@ -162,7 +163,10 @@ const DishDetail = (props) => {
           </div>
           <div className="row">
               <RenderDish dish= {props.dish} />
-              <RenderComments comments={props.comments} />
+              <RenderComments comments={props.comments}
+                addComment={props.addComment}
+                dishId={props.dish.id}
+              />
           </div>
       </div>
     )
